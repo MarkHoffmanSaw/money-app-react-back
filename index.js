940d17388b8bfb6d8c9300c55282388eedc2e815
Artur <tullers@mail.ru>
2021-11-25 01:08:17 +0300

    fix fetching

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import express, { json } from "express";
 import fetch from "node-fetch";
-
-const PORT = process.env.PORT || 5000;
+import cors from "cors";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(json());
 
 app.get("/currencies", async (req, res) => {
   try {
-    const res = await fetch(
+    const result = await fetch(
       "https://api.simpleswap.io/v1/get_all_currencies?api_key=1eb05f06-53ae-4e11-a34a-a66bc963d9fc"
     );
 
-    console.log(res);
+    const data = await result.json();
 
-    const data = await res.json();
-
-    console.log(data);
-  } catch (error) {
-    console.log(error);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
   }
 });
 
