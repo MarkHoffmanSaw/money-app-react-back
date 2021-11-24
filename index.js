222db06e05b1b2ffa16e729ba037b3f9474d264b
Artur <tullers@mail.ru>
2021-11-24 23:08:10 +0300

    fix search + reset + fetch

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express, { json } from "express";
+import fetch from "node-fetch";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(json());
+app.get("/currencies", async (req, res) => {
+  try {
+    const res = await fetch(
+      "https://api.simpleswap.io/v1/get_all_currencies?api_key=1eb05f06-53ae-4e11-a34a-a66bc963d9fc"
+    );
 
-app.get("/currencies");
+    console.log(res);
+
+    const data = await res.json();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
